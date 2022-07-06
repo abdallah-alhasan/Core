f9224a09efbcf703c1bf8481dfeb5e5821a2d798
AmalAl-Abed <amalabed116@gmail.com>
2022-07-07 01:13:13 +0300

    main

--- React/src/App.js
+++ React/src/App.js
@@ -6,6 +6,8 @@ import Appointments from './Pages/Appointments';
 import Register from './Pages/Register';
 import Login from './Pages/Login'
 import Profile from './Pages/Profile'
+import About from './Pages/About'
+import ContactUs from './Pages/ContactUs';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<ContactUs />} />
           <Route path='/book-an-appointment' element={<Appointments />} />
           {/* enter your routes here */}
           <Route path='*' element={<Error />} />
--- React/src/layouts/Footer.js
+++ React/src/layouts/Footer.js
@@ -3,13 +3,14 @@ import React from 'react'
 function Footer() {
   return (
     <React.Fragment>
-            <footer class="footer-2">        
+              <footer class="footer-2">           
+        <div class="footer-pri">             
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4">
-                        <div class="widget widget-about">       
-                            <a href="index-hospital-classic.html">
-                                <img src="images/logo-light.svg" class="logo-footer" alt=""/>
+                        <div class="widget widget-about">           
+                            <a href="/ndex-hospital-classic.html">
+                                <img src="images/logo-light.svg" class="logo-footer" alt="" />
                             </a>
                             <p class="mt-20 footer-text">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
                             <hr class="mt-30 mb-30" />
@@ -29,50 +30,50 @@ function Footer() {
                             <hr class="mt-30 mb-30" />
                             <h5 class="heading font-bold mb-20">Social Connect</h5>
                             <ul class="social social-round social-2x">
-                                <li><a class="facebook" href="#"><i class="ion-logo-facebook"></i></a></li>
-                                <li><a class="twitter" href="#"><i class="ion-logo-twitter"></i></a></li>
-                                <li><a class="google" href="#"><i class="ion-logo-google"></i></a></li>
-                                <li><a class="youtube" href="#"><i class="ion-logo-youtube"></i></a></li>
-                                <li><a class="whatsapp" href="#"><i class="ion-logo-whatsapp"></i></a></li>
+                                <li><a class="facebook" href="/"><i class="ion-logo-facebook"></i></a></li>
+                                <li><a class="twitter" href="/"><i class="ion-logo-twitter"></i></a></li>
+                                <li><a class="google" href="/"><i class="ion-logo-google"></i></a></li>
+                                <li><a class="youtube" href="/"><i class="ion-logo-youtube"></i></a></li>
+                                <li><a class="whatsapp" href="/"><i class="ion-logo-whatsapp"></i></a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <hr class="mt-30 mb-30 d-lg-none" />
-                        <div class="widget widget-links">        
+                        <div class="widget widget-links">           
                             <h5 class="heading font-bold">Useful Links</h5>
                             <ul>
-                                <li><a href="book-appointment-form.html">Book Appoitment</a></li>
-                                <li><a href="departments-style-1.html">View Departments</a></li>
-                                <li><a href="contact-1.html">Our Locations</a></li>
-                                <li><a href="doctors-style-2.html">Doctors On Panel</a></li>
-                                <li><a href="why-us-1.html">Facilities Available</a></li>
-                                <li><a href="about-us-1.html">About Hospital</a></li>
-                                <li><a href="policies.html">Hospital Rules</a></li>
+                                <li><a href="/ook-appointment-form.html">Book Appoitment</a></li>
+                                <li><a href="/epartments-style-1.html">View Departments</a></li>
+                                <li><a href="/ontact-1.html">Our Locations</a></li>
+                                <li><a href="/octors-style-2.html">Doctors On Panel</a></li>
+                                <li><a href="/hy-us-1.html">Facilities Available</a></li>
+                                <li><a href="/bout-us-1.html">About Hospital</a></li>
+                                <li><a href="/olicies.html">Hospital Rules</a></li>
                             </ul>
                         </div>
                         <hr class="mt-30 mb-30" />
-                        <div class="widget widget-twitter">        
+                        <div class="widget widget-twitter">            
                             <h5 class="heading font-bold">Latest Tweet</h5>
                             <div class="tweets">
                                 <ul class="carousel-items">
                                     <li class="tweet-item">
-                                        <p>How to tell if the hand sanitizer you’re buying is safe and actually works? <a href="#">#askDoctor</a></p>
+                                        <p>How to tell if the hand sanitizer you’re buying is safe and actually works? <a href="/">#askDoctor</a></p>
                                     </li>
                                     <li class="tweet-item">
-                                        <p>Screen time doesn’t hurt kids’ social skills, says harvard university <a href="#">#healthcare #dailyTips</a></p>
+                                        <p>Screen time doesn’t hurt kids’ social skills, says harvard university <a href="/">#healthcare #dailyTips</a></p>
                                     </li>
                                     <li class="tweet-item">
-                                        <p>Can clothes and shoes track infection into your house? What to Know <a href="#">Read blog here</a></p>
+                                        <p>Can clothes and shoes track infection into your house? What to Know <a href="/">Read blog here</a></p>
                                     </li>
                                 </ul>
                             </div>
-                            <a href="#" class="btn btn-outline-light curved btn-sm mt-10">Follow Us</a>
+                            <a href="/" class="btn btn-outline-light curved btn-sm mt-10">Follow Us</a>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <hr class="mt-30 mb-30 d-lg-none" />
-                        <div class="widget widget-timings">          
+                        <div class="widget widget-timings">           
                             <h5 class="heading font-bold">Hospital Timings</h5>
                             <table class="table table-bordered footer-text">
                                 <tr>
@@ -90,12 +91,12 @@ function Footer() {
                             </table>
                         </div>
                         <hr class="mt-30 mb-30" />
-                        <div class="widget widget-subscribe">      
+                        <div class="widget widget-subscribe">          
                             <h5 class="heading font-bold">Subscribe Newsletter</h5>
                             <p class="footer-text">Subscribe to our newsletter for daily health tips</p>
                             <form class="search">  
                                 <div class="input-group">
-                                    <input type="text" class="form-control curved style-1" placeholder="Your Email Id"/>
+                                    <input type="text" class="form-control curved style-1" placeholder="Your Email Id" />
                                     <div class="input-group-append">
                                         <button class="btn btn-primary curved" type="button">Send</button>
                                     </div>
@@ -103,49 +104,49 @@ function Footer() {
                             </form>
                         </div>
                         <hr class="mt-30 mb-30" />
-                        <div class="widget widget-pay">        
+                        <div class="widget widget-pay">           
                             <h5 class="heading font-bold">Payment Accepted</h5>
                             <ul class="payment-list">
                                 <li class="item">
-                                    <img src="images/payment-type-1.png" alt=""/>
+                                    <img src="images/payment-type-1.png" alt="" />
                                 </li>
                                 <li class="item">
-                                    <img src="images/payment-type-2.png" alt=""/>
+                                    <img src="images/payment-type-2.png" alt=""  />
                                 </li>
                                 <li class="item">
-                                    <img src="images/payment-type-3.png" alt=""/>
+                                    <img src="images/payment-type-3.png" alt="" />
                                 </li>
                                 <li class="item">
-                                    <img src="images/payment-type-4.png" alt=""/>
+                                    <img src="images/payment-type-4.png" alt="" />
                                 </li>
                                 <li class="item">
-                                    <img src="images/payment-type-5.png" alt=""/>
+                                    <img src="images/payment-type-5.png" alt="" />
                                 </li>
                             </ul> 
                         </div>
                     </div>
                 </div>
             </div>
-      
+        </div>
         <div class="footer-sec">          
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 mt-10 mb-10">
                         <ul class="misc-links">
                             <li>
-                                <a href="#">Privacy Policy</a>
+                                <a href="/">Privacy Policy</a>
                             </li>
                             <li>
-                                <a href="#">Terms & Conditions</a>
+                                <a href="/">Terms & Conditions</a>
                             </li>
                             <li>
-                                <a href="#">Usage Rights</a>
+                                <a href="/">Usage Rights</a>
                             </li>
                         </ul>
                     </div>
                     <div class="col-lg-6 mt-10 mb-10 text-right">
                         <p class="mb-0 footer-text text-lg-right text-center">&copy; 2021 All Rights Reserved.
-                            <a href="http://wwsthemes.com/" target="_blank" class="font-semi-bold">WWS THEMES</a>
+                     
                         </p>
                     </div>
                 </div>
